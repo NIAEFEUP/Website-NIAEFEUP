e3f2804737b6103b8f331ad27f3df5a5867190dc
PeaceOff <davidazevedo1996@hotmail.com>
2017-03-09 16:30:45 +0000

    Added inline customData expansion

--- controllers/members.js
+++ controllers/members.js
@@ -16,7 +16,7 @@ Grupos:
 direção : https://api.stormpath.com/v1/groups/5dd3fxA6icR2V6G1dIoRB3
 membros: https://api.stormpath.com/v1/groups/5jYgpzJVqUAQtyZmMYKDyD
 recrutas : https://api.stormpath.com/v1/groups/5vDGIOEzmRt82YUiPV3ygr
-
+*/
 var dir_group_ref = 'https://api.stormpath.com/v1/groups/5dd3fxA6icR2V6G1dIoRB3';
 var mem_group_ref = 'https://api.stormpath.com/v1/groups/5jYgpzJVqUAQtyZmMYKDyD';
 var rec_group_ref = 'https://api.stormpath.com/v1/groups/5vDGIOEzmRt82YUiPV3ygr';
@@ -24,20 +24,20 @@ var rec_group_ref = 'https://api.stormpath.com/v1/groups/5vDGIOEzmRt82YUiPV3ygr'
 var stormpath = require('stormpath');
 var client = new stormpath.Client();
 
+var query = {//You need this to expand the customData
+    expand : 'customData'
+};
+
 client.getGroup(dir_group_ref, function (err,group){
 
-  group.getAccounts({}, function (err, collection) {
+  group.getAccounts(query, function (err, collection) {
       collection.each(function (account, next) {
           console.log('Found account for ' + account.givenName + ' (' + account.email + ')');
-          console.log(account);
-          account.getCustomData(function(err,data){
-            console.log(data);
-          });
-          next();
+          console.log(account.customData);
       });
   });
 });
-END_TESTING*/
+/*END_TESTING*/
 
 /* GET Template listing. */
 router.get('/', function(req, res) {
