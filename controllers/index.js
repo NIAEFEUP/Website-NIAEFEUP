5f676ddbab5576e8e88a6e51b75171dd7e2a48b4
Pedro Costa <pedrodc23@gmail.com>
2017-08-27 21:01:06 +0100

    Restructured code

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  home: require('./home'),
-  members: require('./members'),
-  profile: require('./profile')() //Needs the () after the function, because you need to invoke the express's router.
-};
