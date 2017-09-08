440dd04d47260a91be555d8cf539874d4efce290
Pedro Costa <pedrodc23@gmail.com>
2017-09-08 18:22:23 +0100

    Updating user info on profile page

--- routes/views/profile.js
+++ routes/views/profile.js
@@ -11,27 +11,41 @@ exports = module.exports = function(req, res) {
 };
 
 /**
- * Get User by ID
+ * Update user profile
  */
 exports.update = function(req, res) {
-    User.model.findById(req.body.id).exec(function(err, item) {
+    User.model.findById(res.locals.user.id).exec(function(err, item) {
 
-        if (err) return res.apiError('database error', err);
-        if (!item) return res.apiError('not found');
+        if (err) {
+            req.flash('warning', 'Database error!');
+        }
+        if (!item) {
+            req.flash('warning', 'User not found!');
+        }
 
-        var data = (req.method == 'POST') ? req.body : req.query;
+        var formData = {
+            avatar: req.body.avatar,
+            name: { first: req.body.first, last: req.body.last },
+            linkedin: req.body.linkedin,
+            github: req.body.github,
+            website: req.body.website,
+            about: req.body.about
+        }
 
-        item.getUpdateHandler(req).process(data, function(err) {
+        var data = (req.method == 'POST') ? formData : req.query;
 
-            if (err) return res.apiError('create error', err);
-
-            res.apiResponse({
-                user: item
-            });
-
+        item.getUpdateHandler(req).process(data, {
+            flashErrors: true,
+        }, function(err) {
+            if (err) {
+                req.flash('warning', 'Error updating profile!');
+                locals.validationErrors = err.errors;
+            } else {
+                req.flash('success', 'Your profile has been updated!');
+                return res.redirect('/profile');
+            }
+            next();
         });
 
     });
-
-    res.render('profile');
 }
