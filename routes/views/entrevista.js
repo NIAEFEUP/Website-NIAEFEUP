15d533d5c6869f744c0faa7ff3840fdd1c7a0156
David Azevedo <davidazevedo1996@hotmail.com>
2017-10-01 22:31:37 +0100

    Finished entrevista form
    The form is available at "/entrevistas" and after a candidate selection. You must be logged in into an account with Member position or higher

--- routes/views/entrevista.js
+++ routes/views/entrevista.js
@@ -14,11 +14,33 @@ exports = module.exports = function(req, res) {
     // Load the current post
     view.on('init', function(next) {
 
-        var q = keystone.list('Candidatura').model.findById(req.params.id);
+        var cand = keystone.list('Candidatura').model.findById(req.params.id);
+        locals.candidato_id = req.params.id;
 
-        q.exec(function(err, result) {
+        cand.exec(function(err, result) {
+
+            if(!result){
+              req.flash('error','Ocorreu um erro, tenta mais tarde!');
+              next(err);
+            }
+
+            if(result.entrevista){//Já foi entrevistado
+
+                var entre = keystone.list('Entrevista').model.findById(req.params.id);
+
+                entre.exec(function(err, result) {
+                    if(!result){
+                      req.flash('error','Ocorreu um erro, tenta mais tarde!');
+                      next(err);
+                    } else {
+                      locals.entrevista = result;
+                      next();
+                    }
+                });
+            }
+
             locals.candidato = result;
-            next(err);
+            next();
         });
 
     });
@@ -26,3 +48,40 @@ exports = module.exports = function(req, res) {
     // Render the view
     view.render('entrevista');
 };
+
+
+exports.create = function(req, res, next) {
+
+  var info = req.body;
+  info.data = new Date();
+
+  //TODO ver o que falta aqui;
+  console.log(info);
+
+  var novaEntr = new Entrevista.model(info);
+
+  novaEntr.save(function(err){
+    if(err){
+
+      if (err.name === 'MongoError' && err.code === 11000){
+
+        req.flash('error', 'Esta entrevista já foi realizada!');
+        res.redirect('/entrevistas');
+
+      } else {
+
+        req.flash('error', 'Ocorreu um erro, por favor tenta novamente!');
+        res.redirect('/entrevistas');
+      }
+
+    } else {
+
+      //TODO atualizar a candidatura com entrevista:feita
+      req.flash('success', 'Entrevista submetida, Obrigado!');
+      res.redirect('/');
+
+    }
+
+    next();
+  });
+}
