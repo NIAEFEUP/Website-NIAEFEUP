0ea4ee267851b40c4a406173ef7f50e98785d07e
Marcelo Ferreira <pount@sapo.pt>
2017-10-04 08:20:58 +0100

    invitations

--- routes/views/candidatura.js
+++ routes/views/candidatura.js
@@ -30,13 +30,13 @@ exports.create = function(req, res, next) {
       }
 
     } else {
-
+/*
       new keystone.Email({
         templateName: 'notificacao_candidatura.pug',
         transport: 'mailgun',
       }).send({
         name: req.body['name.first'],
-        surname: req.body['name.second'],
+        surname: req.body['name.last'],
       },{
         to: req.body['email'],
         from: {
@@ -52,6 +52,31 @@ exports.create = function(req, res, next) {
               console.log('Successfully sent Mailgun test with result:\n', result);
             }
       });
+*/
+new keystone.Email({
+  templateName: 'convite.pug',
+  transport: 'mailgun',
+}).send({
+  name: req.body['name.first'],
+  surname: req.body['name.last'],
+  link_google_groups: process.env.GOOGLE_GROUPS_INVITE,
+  link_google_drive: process.env.GOOGLE_DRIVE_INVITE,
+},{
+  to: 'pount@sapo.pt', //req.body['email']
+  from: {
+    name: 'NIAEFEUP',
+    email: 'excited@samples.mailgun.org',
+  },
+  subject: 'Candidatura subemetida.',
+
+}, function (err, result) {
+      if (err) {
+        console.error('Mailgun test failed with error:\n', err);
+      } else {
+        console.log('Successfully sent Mailgun test with result:\n', result);
+      }
+});
+
 
       req.flash('success', 'Candidatura submetida, Obrigado!');
       res.redirect('/');
