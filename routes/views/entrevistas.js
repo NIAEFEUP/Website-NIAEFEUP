1c723cf13ea08c43ebdca7124102c965a0f1c0da
Marcelo Ferreira <pount@sapo.pt>
2017-10-05 13:54:20 +0100

    already send the invitations
    for now only works for one user accepted

--- routes/views/entrevistas.js
+++ routes/views/entrevistas.js
@@ -34,52 +34,58 @@ exports = module.exports = function(req, res) {
 
 exports.approve = function(req, res) {
 
-  for (var i = 0; i < req.length; i++) {
-    var url = 'https://slack.com/api/users.admin.invite?token=' + process.env.SLACK_INVITE  + '&email=consordlbaw@gmail.com'; // mudar para o email da pessoa
+   var request_ID;
 
-    // send request for send slack invitation using slack Web API
-    https.get(url,(resp) => {
-        let data = '';
-        resp.on('data', (chunk) => {
-          data += chunk;
+  Candidatura.model.find({"_id":req.body['accept']}, 'name email').exec(function(err, results){
+
+    for (var i = 0; i < results.length; i++) {
+      var url = 'https://slack.com/api/users.admin.invite?token=' + process.env.SLACK_INVITE  + '&email=up201405323@fe.up.pt'; // mudar para o email da pessoa
+
+      // send request for send slack invitation using slack Web API
+      https.get(url,(resp) => {
+          let data = '';
+          resp.on('data', (chunk) => {
+            data += chunk;
+          });
+
+          resp.on('end', () => {
+            console.log(data);
+            }
+          });
+
+        }).on("error", (err) => {
+          console.log("Error: " + err.message);
         });
 
-        resp.on('end', () => {
-          console.log(JSON.parse(data).explanation);
-        });
+        // send email with invites for google groups and google drive
+      new keystone.Email({
+        templateName: 'convite.pug',
+        transport: 'mailgun',
+      }).send({
+        name: results[i]['name']['first'],
+        surname: results[i]['name']['last'],
+        link_google_groups: process.env.GOOGLE_GROUPS_INVITE,
+        link_google_drive: process.env.GOOGLE_DRIVE_INVITE,
+      },{
+        to: 'pount@sapo.pt', //req.body['email']
+        from: {
+          name: 'NIAEFEUP',
+          email: 'excited@samples.mailgun.org',
+        },
+        subject: 'Convite para Google Groups e Google Drive',
 
-      }).on("error", (err) => {
-        console.log("Error: " + err.message);
+      }, function (err, result) {
+        if (err) {
+          console.error('Mailgun test failed with error:\n', err);
+        } else {
+          console.log('Successfully sent Mailgun test with result:\n', result);
+        }
       });
+    }
+  });
 
-      // send email with invites for google groups and google drive
-    new keystone.Email({
-      templateName: 'convite.pug',
-      transport: 'mailgun',
-    }).send({
-      name: req.body['name.first'],
-      surname: req.body['name.last'],
-      link_google_groups: process.env.GOOGLE_GROUPS_INVITE,
-      link_google_drive: process.env.GOOGLE_DRIVE_INVITE,
-    },{
-      to: 'pount@sapo.pt', //req.body['email']
-      from: {
-        name: 'NIAEFEUP',
-        email: 'excited@samples.mailgun.org',
-      },
-      subject: 'Convite para Google Groups e Google Drive',
-
-    }, function (err, result) {
-      if (err) {
-        console.error('Mailgun test failed with error:\n', err);
-      } else {
-        console.log('Successfully sent Mailgun test with result:\n', result);
-      }
-    });
-
-  }
     //TODO Passar os membros para User Recruta e enviar os dados para slack e etc por email
     //TODO se for apenas 1 entao accept não é uma array mas sim um valor (id do candidato)
-    console.log(req.body);
+
     res.redirect('/entrevistas');
 }
