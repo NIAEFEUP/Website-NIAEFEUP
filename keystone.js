eda34f6e5d3edbf2d53d9d18548270ddac0af8ea
Marcelo Ferreira <pount@sapo.pt>
2017-10-06 12:28:42 +0100

    invitations done and some refractoring

--- keystone.js
+++ keystone.js
@@ -50,22 +50,8 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-    enquiries: 'enquiries',
     users: 'users',
 });
-
-// Start Keystone to connect to your database and initialise the web server
-
-
-if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
-    console.log('----------------------------------------' +
-        '\nWARNING: MISSING MAILGUN CREDENTIALS' +
-        '\n----------------------------------------' +
-        '\nYou have opted into email sending but have not provided' +
-        '\nmailgun credentials. Attempts to send will fail.' +
-        '\n\nCreate a mailgun account and add the credentials to the .env file to' +
-        '\nset up your mailgun integration');
-}
 
 
 keystone.start();
--- routes/views/entrevistas.js
+++ routes/views/entrevistas.js
@@ -1,6 +1,7 @@
 var keystone = require('keystone');
 var Entrevista = keystone.list('Entrevista');
 var Candidatura = keystone.list('Candidatura');
+const nodemailer = require('nodemailer');
 const https = require('https');
 //var $  = require('jquery');
 //var dt = require('datatables.net')( window, $ );
@@ -34,12 +35,10 @@ exports = module.exports = function(req, res) {
 
 exports.approve = function(req, res) {
 
-   var request_ID;
-
-  Candidatura.model.find({"_id":req.body['accept']}, 'name email').exec(function(err, results){
+  Candidatura.model.find({'_id': {$in:req.body['accept']}}).exec(function(err, results){
 
     for (var i = 0; i < results.length; i++) {
-      var url = 'https://slack.com/api/users.admin.invite?token=' + process.env.SLACK_INVITE  + '&email=up201405323@fe.up.pt'; // mudar para o email da pessoa
+      var url = 'https://slack.com/api/users.admin.invite?token=' + process.env.SLACK_INVITE  + '&email=' + results[i]['email']; // mudar para o email da pessoa
 
       // send request for send slack invitation using slack Web API
       https.get(url,(resp) => {
@@ -51,39 +50,43 @@ exports.approve = function(req, res) {
           resp.on('end', () => {
             console.log(data);
             }
-          });
+          );
 
         }).on("error", (err) => {
           console.log("Error: " + err.message);
         });
 
-        // send email with invites for google groups and google drive
-      new keystone.Email({
-        templateName: 'convite.pug',
-        transport: 'mailgun',
-      }).send({
-        name: results[i]['name']['first'],
-        surname: results[i]['name']['last'],
-        link_google_groups: process.env.GOOGLE_GROUPS_INVITE,
-        link_google_drive: process.env.GOOGLE_DRIVE_INVITE,
-      },{
-        to: 'pount@sapo.pt', //req.body['email']
-        from: {
-          name: 'NIAEFEUP',
-          email: 'excited@samples.mailgun.org',
-        },
-        subject: 'Convite para Google Groups e Google Drive',
+        var transporter = nodemailer.createTransport({
+          service: 'Gmail',
+          auth: {
+            user: process.env.GMAIL_ADDRESS,
+            pass: process.env.GMAIL_PASS
+          }
+        });
 
-      }, function (err, result) {
-        if (err) {
-          console.error('Mailgun test failed with error:\n', err);
+        var message = "<p> Olá " + results[i]['name']['first'] + " " + results[i]['name']['last'] + " </p>";
+        message += " <p> Para aderires ao google groups, clica no link a baixo: </p>";
+        message += " <a href=" + process.env.GOOGLE_GROUPS_INVITE + "> Google Groups</a>";
+        message += " <p> Para aderires ao google drive, clica no link a baixo: </p>";
+        message += " <a href=" + process.env.GOOGLE_DRIVE_INVITE + "> Google Drive</a>";
+
+        var mailOptions = {
+          from: process.env.GMAIL_ADDRESS, //TODO mudar para o email do NI
+          to:results[i]['email'],
+          subject: 'Convite para Google Groups e Google Drive',
+          html: message
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
         } else {
-          console.log('Successfully sent Mailgun test with result:\n', result);
+          console.log('Email sent: ' + info.response);
         }
       });
+
     }
   });
-
     //TODO Passar os membros para User Recruta e enviar os dados para slack e etc por email
     //TODO se for apenas 1 entao accept não é uma array mas sim um valor (id do candidato)
 
