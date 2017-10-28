b8d9d4f3f35abd8231b393c63e28004abe40e477
Pedro Câmara <camara-pedro@hotmail.com>
2017-10-28 23:56:32 +0100

    Fixes issue #56

---
+++ updates/0.0.2-resize-photos.js
@@ -0,0 +1,31 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const util = require('util');
+const writeFile = util.promisify(fs.writeFile);
+
+const imagesPath = './public/uploads/members/';
+
+const resizeImage = function (file) {
+	return sharp(`${imagesPath}${file}`)
+	.resize(512, 512)
+	.crop(sharp.gravity.center)
+	.jpeg()
+	.toBuffer()
+	.then((buffer) => {
+		return writeFile(`${imagesPath}${file}`, buffer);
+	});
+};
+
+exports.create = function (done) {
+	const promise = Promise.resolve();
+	fs.readdirSync(imagesPath).forEach(file => {
+		if (file.toLowerCase().includes('.jpg') || file.toLowerCase().includes('.jpeg')) {
+			promise
+			.then(() => resizeImage(file))
+			.catch((err) => {
+				console.error(`Error on file "${file}" -`, err);
+			});
+		}
+	});
+	promise.then(done);
+};
