de6d12def3b80d5fdbe6fb2b122e97bae5928c14
David Azevedo <davidazevedo1996@hotmail.com>
2018-01-02 09:38:30 +0000

    Fixed connectivity timeout issues

--- keystone.js
+++ keystone.js
@@ -10,26 +10,32 @@ var keystone = require('keystone');
 // and documentation.
 
 keystone.init({
-    'name': 'NIAEFEUP',
-    'brand': 'NIAEFEUP',
-    'sass': 'public',
-    'static': 'public',
-    'favicon': 'public/favicon.ico',
-    'views': 'templates/views',
-    'view engine': 'pug',
-    'view cache': true,
-    'emails': 'templates/emails',
-    'signin logo': ['../../images/favicon-96x96.png'],
-    'signin redirect': '/',
-    'signout redirect': '/',
-    'signin url': '/signin',
-    'signout url': '/',
-    'auto update': true,
-    'session': true,
-    'session store': 'mongo',
-    'auth': true,
-    'user model': 'User',
-    'mongo options': { server: { keepAlive: 1 }},
+	'name': 'NIAEFEUP',
+	'brand': 'NIAEFEUP',
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'pug',
+	'view cache': true,
+	'emails': 'templates/emails',
+	'signin logo': ['../../images/favicon-96x96.png'],
+	'signin redirect': '/',
+	'signout redirect': '/',
+	'signin url': '/signin',
+	'signout url': '/',
+	'auto update': true,
+	'session': true,
+	'session store': 'mongo',
+	'auth': true,
+	'user model': 'User',
+	'mongo options': {
+		server: {
+			keepAlive: 1,
+			reconnectTries: Number.MAX_VALUE,
+			reconnectInterval: 1500,
+		},
+	},
 });
 
 // Load your project's Models
@@ -39,10 +45,10 @@ keystone.import('models');
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
-    _: require('lodash'),
-    env: keystone.get('env'),
-    utils: keystone.utils,
-    editable: keystone.content.editable,
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
 });
 
 // Load your project's Routes
@@ -51,15 +57,15 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-    users: 'users',
+	users: 'users',
 });
 
 if (!process.env.RECRUTAMENTO)
-    console.log("WARNING: RECRUTAMENTO variable not set in env file!");
+	{ console.log('WARNING: RECRUTAMENTO variable not set in env file!'); }
 
 if (!process.env.GMAIL_PASS || !process.env.GMAIL_ADDRESS)
-    console.log("WARNING: You must define GMAIL_PASS and GMAIL_ADDRESS in the env file for the email notifier to work!");
+	{ console.log('WARNING: You must define GMAIL_PASS and GMAIL_ADDRESS in the env file for the email notifier to work!'); }
 
 if (!process.env.SLACK_INVITE || !process.env.GOOGLE_DRIVE_INVITE || !process.env.GOOGLE_GROUPS_INVITE)
-    console.log("WARNING: You must define GOOGLE_GROUPS_INVITE, GOOGLE_DRIVE_INVITE and SLACK_INVITE in the env file for the accept candidate to work!");
+	{ console.log('WARNING: You must define GOOGLE_GROUPS_INVITE, GOOGLE_DRIVE_INVITE and SLACK_INVITE in the env file for the accept candidate to work!'); }
 keystone.start();
