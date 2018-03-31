36aa6f73dc9fdf55c5ea29fc24267179910ef36f
Pedro Costa <pedrodc23@gmail.com>
2018-03-31 19:57:04 +0100

    Upload fotos to project from AdminUI

--- models/Project.js
+++ models/Project.js
@@ -11,13 +11,24 @@ var Project = new keystone.List('Project', {
 	autokey: { path: 'slug', from: 'title', unique: true },
 });
 
+var localStorage = new keystone.Storage({
+	adapter: keystone.Storage.Adapters.FS,
+	fs: {
+		path: './public/images/projects/',
+		publicPath: '/images/projects/',
+	},
+});
+
 Project.add({
 	title: { type: String, required: true },
 	type: { type: Types.Select, options: 'project, workshop, event', default: 'project', index: true },
 	state: { type: Types.Select, options: 'planning, working, completed', default: 'planning', index: true },
 	responsible: { type: Types.Relationship, ref: 'User', index: true },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'completed' } },
-	image_path: { type: String, label: 'Image path on disk', default: '/images/projects/default_project.jpg' },
+	image: {
+		type: Types.File,
+		storage: localStorage,
+	},
 	description: { type: Types.Html, wysiwyg: true, height: 150 },
 });
 
