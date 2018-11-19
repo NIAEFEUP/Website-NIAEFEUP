d3430c73361a8be9e372ca7ce6463343f9402a61
Miguel Duarte <miguelpduarte98@hotmail.com>
2018-11-19 00:22:43 +0000

    Started refactoring to use email wrapper instead of nodemailer directly

--- routes/views/email.js
+++ routes/views/email.js
@@ -1,7 +1,7 @@
 const keystone = require('keystone');
 const Candidato = keystone.list('Candidato');
 const FaseCandidatura = keystone.list('FaseCandidatura');
-const nodemailer = require('nodemailer');
+const email_wrapper = require('../utils/email_wrapper');
 
 exports = module.exports = function (req, res) {
 	let view = new keystone.View(req, res);
@@ -22,56 +22,24 @@ exports.send = function (req, res, next) {
 					req.flash('error', 'Ocorreu um erro, tenta mais tarde!');
 					next(err);
 				} else if (items) {
-					if (process.env.GMAIL_ADDRESS && process.env.GMAIL_PASS) {
-						let transporter = nodemailer.createTransport({
-							service: 'Gmail',
-							auth: {
-								user: process.env.GMAIL_ADDRESS,
-								pass: process.env.GMAIL_PASS,
-							},
-						});
+					const emails_candidatos = items.map(item => item.email);
 
-						let items_email = [];
+					const receivers = emails_candidatos.join(' , ');
+					console.log('SENDING TO:', receivers, emails_candidatos);
 
-						for (let i = 0; i < items.length; i++) {
-							items_email.push(items[i].email);
-						}
+					const text = req.body.email_text.replace(/\r?\n/g, '<br />');
 
-						let receivers = items_email.join(' , ');
-						console.log('SENDING TO:');
-						console.log(receivers);
-						let text = req.body.email_text.replace(/\r?\n/g, '<br />');
+					// Why?
+					const message = '<div> ' + text + '</div>';
 
-						let message = '<div> ' + text + '</div>';
+					let mailOptions = {
+						from: process.env.GMAIL_ADDRESS,
+						bcc: emails_candidatos,
+						subject: req.body.email_subject,
+						html: message,
+					};
 
-						// signature
-						message += '<br><br>';
-						message += '<div style=\'float:left;\'><img src=\'cid:id_1234698\' alt=\'logo niaefeup\' title=\'logo\' style=\'display:block\' width=\'50\'></div><div style=\'padding-left:70px\'><h2>Núcleo de Informática da AEFEUP</h2>';
-						message += '<p><a href=\'ni@aefeup.pt\'>ni@aefeup.pt</a></p>';
-						message += '<p><a href=\'https://ni.fe.up.pt\'>Website</a> | <a href=\'https://www.facebook.com/NIAEFEUP\'>Facebook</a> | <a href=\'https://www.instagram.com/niaefeup/\'>Instagram</a></p>';
-						message += '<p> Sala B315, Rua Dr.Roberto Frias, s/n 4200-465 Porto Portugal | <a href=\'https://goo.gl/maps/aj2LBqFkwjx\'>Google Maps</a></p>';
-						message += '</div>';
-
-						let mailOptions = {
-							from: process.env.GMAIL_ADDRESS,
-							bcc: receivers,
-							subject: req.body.email_subject,
-							html: message,
-							attachments: [{
-								filename: 'logo-niaefeup.png',
-								path: 'https://ni.fe.up.pt/images/logo-niaefeup.png',
-								cid: 'id_1234698',
-							}],
-						};
-
-						transporter.sendMail(mailOptions, function (error, info) {
-							if (error) {
-								console.log(error);
-							} else {
-								console.log('Email sent: ' + info.response);
-							}
-						});
-					}
+					email_wrapper.sendMail(mailOptions);
 				}
 			});
 		}
@@ -79,5 +47,4 @@ exports.send = function (req, res, next) {
 
 	req.flash('success', 'Email enviado com sucesso');
 	res.redirect('/');
-
 };
