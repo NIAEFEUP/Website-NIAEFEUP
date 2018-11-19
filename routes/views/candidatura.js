6e74ba4426539573c49dfa13b01afcb996a9b824
Miguel Duarte <miguelpduarte98@gmail.com>
2018-11-19 17:38:14 +0000

    Fix/nodemailer 181 (#182)
    * Added email wrapper util file
    
    * Started refactoring to use email wrapper instead of nodemailer directly
    
    * Refactored all usages of nodemailer to use email_wrapper instead - needs bulk testing
    
    * Final fixes, ready to deploy
    
    * Removed random console logs

--- routes/views/candidatura.js
+++ routes/views/candidatura.js
@@ -2,7 +2,7 @@ const keystone = require('keystone');
 const Candidato = keystone.list('Candidato');
 const User = keystone.list('User');
 const FaseCandidatura = keystone.list('FaseCandidatura');
-const nodemailer = require('nodemailer');
+const email_wrapper = require('../utils/email_wrapper');
 
 exports = module.exports = function (req, res) {
 	let view = new keystone.View(req, res);
@@ -52,44 +52,17 @@ exports.create = function (req, res) {
 						res.redirect('/candidatura');
 					}
 				} else {
-					if (process.env.GMAIL_ADDRESS && process.env.GMAIL_PASS) {
-						let transporter = nodemailer.createTransport({
-							service: 'Gmail',
-							auth: {
-								user: process.env.GMAIL_ADDRESS,
-								pass: process.env.GMAIL_PASS,
-							},
-						});
+					let message = '<p> Olá ' + req.body['name.first'] + ' ' + req.body['name.last'] + '</p>';
+					message += '<p> Muito obrigado pelo teu interesse em fazeres parte do Núcleo de Informática. A tua candidatura será revista e esperamos contactar-te brevemente com mais informações!</p>';
 
-						let message = '<p> Olá ' + req.body['name.first'] + ' ' + req.body['name.last'] + '</p>';
-						message += '<p> Muito obrigado pelo teu interesse em fazeres parte do Núcleo de Informática. A tua candidatura será revista e esperamos contactar-te brevemente com mais informações!</p>';
+					let mailOptions = {
+						from: process.env.GMAIL_ADDRESS,
+						to: req.body.email,
+						subject: '[NIAEFEUP] Candidatura submetida',
+						html: message,
+					};
 
-						message += '<div style=\'float:left;\'><img src=\'cid:id_1234698\' alt=\'logo niaefeup\' title=\'logo\' style=\'display:block\' width=\'50\'></div><div style=\'padding-left:70px\'><h2>Núcleo de Informática da AEFEUP</h2>';
-						message += '<p><a href=\'ni@aefeup.pt\'>ni@aefeup.pt</a></p>';
-						message += '<p><a href=\'https://ni.fe.up.pt\'>Website</a> | <a href=\'https://www.facebook.com/NIAEFEUP\'>Facebook</a> | <a href=\'https://www.instagram.com/niaefeup/\'>Instagram</a></p>';
-						message += '<p> Sala B315, Rua Dr.Roberto Frias, s/n 4200-465 Porto Portugal | <a href=\'https://goo.gl/maps/aj2LBqFkwjx\'>Google Maps</a></p>';
-						message += '</div>';
-
-						let mailOptions = {
-							from: process.env.GMAIL_ADDRESS,
-							to: req.body.email,
-							subject: 'Candidatura submetida.',
-							html: message,
-							attachments: [{
-								filename: 'logo-niaefeup.png',
-								path: 'https://ni.fe.up.pt/images/logo-niaefeup.png',
-								cid: 'id_1234698',
-							}],
-						};
-
-						transporter.sendMail(mailOptions, function (error, info) {
-							if (error) {
-								console.log(error);
-							} else {
-								console.log('Email sent: ' + info.response);
-							}
-						});
-					}
+					email_wrapper.sendMail(mailOptions);
 
 					req.flash('success', 'Candidatura submetida, Obrigado!');
 					res.redirect('/');
